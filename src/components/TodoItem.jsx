4f7bf270e2f914e1ff903dfd6c0514f6b81db3ce
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-06-10 21:01:04 +0900

    install pure render mixin.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 import TodoInput from './TodoInput'
 
 export default class TodoItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+  }
+
   render() {
     return(
       <li className="todo">
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 
 import TodoItem from './TodoItem'
 
 export default class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+  }
+
   render() {
     return(
       <section className="main">
